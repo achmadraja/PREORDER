025cf7280128e42cbe2a7172d3b589ca90626f95
Achmad Raja Qodli Zaka <achmadrajaqz@gmail.com>
2023-04-09 22:44:00 +0700

    pesan.php rev

--- pemesanan/pesan.php
+++ pemesanan/pesan.php
@@ -15,7 +15,32 @@
 <body>
     <center>
         <h2>Isi Formulir PreOrder</h2>
-        
+        <table>
+            <form method="POST"
+            action="http://localhost:8080/preorder/pemesanan/proses_pesan.php">
+            <input name="id_barang" type="hidden" value="<?= $id_barang ?>">    
+            <tr>
+                    <td>Nama :</td>
+                    <td><input name="nama" type="text"></td>
+                </tr>
+                <tr>
+                    <td>Email :</td>
+                    <td><input name="email" type="text"></td>
+                </tr>
+                <tr>
+                    <td>No HP :</td>
+                    <td><input name="no_hp" type="text"></td>
+                </tr>
+                <tr>
+                    <td>Alamat :</td>
+                    <td><input name="alamat" type="text"></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><button type="submit">Submit</button></td>
+                </tr>
+            </form>
+        </table>
     </center>
 </body>
 </html>
